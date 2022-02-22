4f2fe912a871905008ca714942218afd12f60a6d
guztus <gustavs.zubars@gmail.com>
2022-02-22 23:18:52 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="First one" />
+      <Todo text="Second one" />
+      <Todo text="Third one" />
+    </div>
+  );
+}
+
+export default App;
